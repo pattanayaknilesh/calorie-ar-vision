8ffd2ad55bc5ef5a0d8d4c8da5a15a9f594bf63d
Nilesh Pattanayak <nilesh.pattanayak@Nileshs-MacBook-Air.local>
2026-04-09 21:09:13 +0530

    Add simulator support to CameraView

--- ios/CalorieArVision/Views/CameraView.swift
+++ ios/CalorieArVision/Views/CameraView.swift
@@ -1,11 +1,35 @@
 import SwiftUI
 import AVFoundation
+import UIKit
 
 struct CameraView: UIViewRepresentable {
     @ObservedObject var viewModel: CameraViewModel
     
     func makeUIView(context: Context) -> UIView {
-        let view = UIView(frame: UIScreen.main.bounds)
+        #if targetEnvironment(simulator)
+        // Simulator Mode: Use a placeholder view
+        let view = UIView()
+        view.backgroundColor = .darkGray
+        
+        let label = UILabel()
+        label.text = "Camera Simulator\n(Using Placeholder)"
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        label.textColor = .white
+        label.frame = view.bounds
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        view.addSubview(label)
+        
+        // Simulate frame capture every 5 seconds
+        Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
+            let mockImage = UIImage(systemName: "fork.knife") ?? UIImage()
+            viewModel.analyzeCurrentFrame(image: mockImage)
+        }
+        
+        return view
+        #else
+        // Physical Device Mode: Use real camera
+        let view = UIView()
         
         let session = AVCaptureSession()
         guard let device = AVCaptureDevice.default(for: .video),
@@ -18,13 +42,14 @@ struct CameraView: UIViewRepresentable {
         }
         
         let previewLayer = AVCaptureVideoPreviewLayer(session: session)
-        previewLayer.frame = view.bounds
         previewLayer.videoGravity = .resizeAspectFill
         view.layer.addSublayer(previewLayer)
         
-        // Add a timer to simulate frame capture every 5 seconds
+        context.coordinator.session = session
+        context.coordinator.previewLayer = previewLayer
+        
         Timer.scheduledTimer(withTimeInterval: 5.0, repeats: true) { _ in
-            capturePhoto(session: session)
+            context.coordinator.capturePhoto(session: session)
         }
         
         DispatchQueue.global(qos: .userInitiated).async {
@@ -32,34 +57,41 @@ struct CameraView: UIViewRepresentable {
         }
         
         return view
+        #endif
     }
     
-    func updateUIView(_ uiView: UIView, context: Context) {}
+    func updateUIView(_ uiView: UIView, context: Context) {
+        #if !targetEnvironment(simulator)
+        context.coordinator.previewLayer?.frame = uiView.bounds
+        #endif
+    }
     
-    private func capturePhoto(session: AVCaptureSession) {
-        let photoOutput = AVCapturePhotoOutput()
-        if session.canAddOutput(photoOutput) {
-            session.addOutput(photoOutput)
-            let settings = AVCapturePhotoSettings()
-            let delegate = PhotoCaptureDelegate { image in
-                self.viewModel.analyzeCurrentFrame(image: image)
-            }
-            // Note: In a real app, we'd keep a reference to the delegate
-            photoOutput.capturePhoto(with: settings, delegate: delegate)
+    func makeCoordinator() -> Coordinator {
+        Coordinator(viewModel: viewModel)
+    }
+    
+    class Coordinator: NSObject, AVCapturePhotoCaptureDelegate {
+        var session: AVCaptureSession?
+        var previewLayer: AVCaptureVideoPreviewLayer?
+        let viewModel: CameraViewModel
+        
+        init(viewModel: CameraViewModel) {
+            self.viewModel = viewModel
         }
-    }
-}
-
-class PhotoCaptureDelegate: NSObject, AVCapturePhotoCaptureDelegate {
-    let completion: (UIImage) -> Void
-    
-    init(completion: @escaping (UIImage) -> Void) {
-        self.completion = completion
-    }
-    
-    func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
-        guard let data = photo.fileDataRepresentation(),
-              let image = UIImage(data: data) else { return }
-        completion(image)
+        
+        func capturePhoto(session: AVCaptureSession) {
+            let photoOutput = AVCapturePhotoOutput()
+            if session.canAddOutput(photoOutput) {
+                session.addOutput(photoOutput)
+                let settings = AVCapturePhotoSettings()
+                photoOutput.capturePhoto(with: settings, delegate: self)
+            }
+        }
+        
+        func photoOutput(_ output: AVCapturePhotoOutput, didFinishProcessingPhoto photo: AVCapturePhoto, error: Error?) {
+            guard let data = photo.fileDataRepresentation(),
+                  let image = UIImage(data: data) else { return }
+            viewModel.analyzeCurrentFrame(image: image)
+        }
     }
 }
